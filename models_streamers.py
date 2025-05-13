5d99dc1050edd8ed682e0814087a0975140dd091
Stevenpm25 <gspena51@ucatolica.edu.co>
2025-05-12 19:25:32 -0500

    arreglos

--- models_streamers.py
+++ models_streamers.py
@@ -1,17 +1,24 @@
-from pydantic import BaseModel, Field
 from typing import Optional
+from sqlmodel import SQLModel, Field
 
-class Streamer(BaseModel):
+
+class StreamerBase(SQLModel):
     name: str = Field(..., min_length=2, max_length=50)
     game: str = Field(..., min_length=2, max_length=50)
     follower_count: int = Field(..., ge=0)
     avg_viewers: int = Field(..., ge=0)
 
-class StreamerWithID(Streamer):
+
+class Streamer(StreamerBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class StreamerWithID(StreamerBase):
     id: int
 
-class UpdatedStreamer(BaseModel):
-    name: Optional[str] = Field(None, min_length=2, max_length=50)
-    game: Optional[str] = Field(None, min_length=2, max_length=50)
-    follower_count: Optional[int] = Field(None, ge=0)
-    avg_viewers: Optional[int] = Field(None, ge=0)
+
+class UpdatedStreamer(SQLModel):
+    name: Optional[str] = Field(default=None, min_length=2, max_length=50)
+    game: Optional[str] = Field(default=None, min_length=2, max_length=50)
+    follower_count: Optional[int] = Field(default=None, ge=0)
+    avg_viewers: Optional[int] = Field(default=None, ge=0)
